22bd9a900d62f18097b686dc18c6b1aa06db8e1c
Srimutia24 <srimutia2121@gmail.com>
2023-03-09 10:13:12 +0700

    srimutia

---
+++ data anggota.js
@@ -0,0 +1,26 @@
+{
+    alert("SELAMAT DATANG DI PERPUSTAKAAN")
+}
+
+// function cekform(){
+//     id anggota = document.getElementById('txtid');
+//     nama = document.getElementById('txtnama');
+//     jk=document.getElementById('jk');
+//     alamat=document.getElementById('txtalamat');
+//     if(nama.value==''){
+//         alert ('nama tidak boleh kosong');
+//         nama.focus();
+//         return false;
+//     }else if(olahraga.value==''){
+//         alert('oalahraga balum dipilih');
+//         olahraga.focus();
+//         return false;
+//     }else if(alamat.value==''){
+//         alert('alamat balum diisi');
+//         alamat.focus();
+//         return false;
+// else{
+//     alert('Terima Kasih Telah mengisi Data Anggota')
+//     return true;
+// }
+// }
